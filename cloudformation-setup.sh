669d0bc8fdbec71d64a136923c8d0e9dc545dddc
alekspo <aleksey.popryaduhin@thedaedals.com>
2017-07-01 15:45:27 +0300

    cf.

--- cloudformation-setup.sh
+++ cloudformation-setup.sh
@@ -4,5 +4,4 @@ aws cloudformation create-stack \
 --stack-name $1  \
 --template-body file://./$2 \
 --region $3 \
---disable-rollback --capabilities="CAPABILITY_IAM" \
---parameters ParameterKey=PipelineAWSAccountId,ParameterValue=089753065094
+--disable-rollback --capabilities="CAPABILITY_IAM"
