9647ce27f3edd47c6a29aaf85faa67463bc18f48
hello-alexpo <aleksey.popryaduhin@yahoo.com>
2017-07-09 17:13:41 +0300

    update.

--- ApiGatewayCloudFormation.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-aws cloudformation create-stack \
---stack-name ApiGatewayCloudFormationStack  \
---template-body file://./ApiGatewayCloudFormation.template \
---region $1 \
---disable-rollback --capabilities="CAPABILITY_IAM"
